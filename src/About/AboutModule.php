78b2a99e3274498b19c99d8994f59c8d8957c018
EstelleBouttier <estellebouttier@gmail.com>
2025-04-11 13:03:14 +0200

    function run

---
+++ src/About/AboutModule.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\About;
+
+class AboutModule
+{
+    
+}
---
+++ src/Home/HomeModule.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Home;
+
+class HomeModule
+{
+    public function Hello()
+    {
+      echo "Bienvenue sur la page d'accueil";
+    }
+     
+}
