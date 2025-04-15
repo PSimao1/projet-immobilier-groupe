7d57e4e3fa3d75a066cd6ab5a38e511301ab56a5
DaDou37 <riera.davi94@gmail.com>
2025-04-15 11:43:35 +0200

    mise en place des dossier

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
+
 require '../vendor/autoload.php';
 
-$app = new \Framework\App([\App\Blog\BlogModule::class]);
+
+use Framework\Renderer\TwigRenderer;
+
+$renderer = new TwigRenderer(dirname(__DIR__) . '/views');
+
+$app = new \Framework\App([\App\Blog\BlogModule::class], ['renderer'=> $renderer]);
+
 $response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
+
+\Http\Response\send($response);
