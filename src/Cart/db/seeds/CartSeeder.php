259380fcb91ae20b6f67c51f6f2868c7cc5ac648
RaizoxDev <thierryalexis.dev@gmail.com>
2025-05-04 16:41:59 +0200

    Refonte DataBase Cart + Liaison Table Properties (Pages Property non modifiées)

--- src/Cart/db/seeds/CartSeeder.php
+++ src/Cart/db/seeds/CartSeeder.php
@@ -22,14 +22,9 @@ class CartSeeder extends AbstractSeed
             $carts[] = [
                 'created_at' => date('Y-m-d H:i:s', $date),
                 'updated_at' => date('Y-m-d H:i:s', $date),           
-                'slug' => $faker->slug(),
                 'total_price' => $faker->randomFloat(2, 0, 100),
-                'arrival_date' => $faker->date('Y-m-d H:i:s', $date),
-                'departure_date' => $faker->date('Y-m-d H:i:s', $date),
                 'payment_method' => $faker->creditCardType(),
-                'located_property' => rand(1,100),
                 'unique_payment_id' => $faker->randomFloat(0, 0, 100),
-                'status' => $faker->randomElement(['En Cours', 'Remboursé', 'Annulé', 'Livré'])
             ];
         }
 
@@ -42,21 +37,16 @@ class CartSeeder extends AbstractSeed
             $userItem = $uItem[$getUser]['id'];
 
             $getProp = array_rand($pItem);
-            $PropItem = $pItem[$getProp]['id'];
+            $propItem = $pItem[$getProp]['id'];
 
             $data[] = [
                 'created_at' => $cart['created_at'],
                 'updated_at' => $cart['updated_at'],
-                'slug' => $cart['slug'],
                 'total_price' => $cart['total_price'],
-                'arrival_date' => $cart['arrival_date'],
-                'departure_date' => $cart['departure_date'],
                 'payment_method' => $cart['payment_method'],
-                'located_property' => $cart['located_property'],
                 'unique_payment_id' => $cart['unique_payment_id'],
-                'status' => $cart['status'],
                 'user_id' => $userItem,
-                'property_id' => $PropItem
+                'property_id' => $propItem,
             ];
         }
 
